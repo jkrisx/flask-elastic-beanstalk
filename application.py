02972335b764e618c3ab9178049260533713056c
jkrisx <joddy.krisnadi@gmail.com>
2021-10-23 13:19:00 +0700

    balikin lagi

--- application.py
+++ application.py
@@ -1,7 +1,6 @@
 from flask import Flask
 application = Flask(__name__)
-app = application
 
-@app.route('/')
+@application.route('/')
 def hello_world():
     return 'Test pake AWS Elastic Beanstalk'
